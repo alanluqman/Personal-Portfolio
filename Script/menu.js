9c62adb485a880b5f9310bcbca087495673670b1
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-06-14 12:55:30 +0300

    Add  HTML+CSS+JavaScript linter

--- Script/menu.js
+++ Script/menu.js
@@ -1,8 +1,8 @@
-let collapsibleMenu = document.querySelector(".menu-slider");
-function collapsible(){
-    collapsibleMenu.classList.toggle("extended");
+const collapsibleMenu = document.querySelector('.menu-slider');
+function collapsible() {
+  collapsibleMenu.classList.toggle('extended');
 }
-let menuBtn = document.querySelector("#menuBtn");
-let xBtn = document.querySelector("#xBtn");
-menuBtn.addEventListener("click",collapsible);
-xBtn.addEventListener("click",collapsible);
+const menuBtn = document.querySelector('#menuBtn');
+menuBtn.addEventListener('click', collapsible);
+const xBtn = document.querySelector('#xBtn');
+xBtn.addEventListener('click', collapsible);
